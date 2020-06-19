7a9cfec557f3193dc5cbf7e158b3fb3a73b230a8
Carter Snook <cartersnook04@gmail.com>
2020-06-19 02:02:01 -0500

    Update Classes.vue

--- src/pages/Classes.vue
+++ src/pages/Classes.vue
@@ -3,15 +3,15 @@
         <div class="wrapper">
             <main>
                 <h1>All Classes</h1>
-                <section class="courses">
-                    <!-- <section v-for="course in data.classes.reverse()" v-bind:key="course.slug">
+                <section class="courses" v-if="data.classes">
+                    <section v-for="course in data.classes.reverse()" v-bind:key="course.slug">
                         <CourseCard
                             :image="course.thumbnail"
                             :title="course.title"
                             :description="course.description"
                             :slug="'classes/' + course.slug"
                         />
-                    </section> -->
+                    </section>
                 </section>
             </main>
         </div>
